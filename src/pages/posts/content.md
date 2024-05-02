ad60abcd613aa78211ef6d2dfb97d4efc751c7ab
Diego Moscaiza <diego-moscaiza@outlook.com>
2024-05-02 15:42:17 -0500

    First Commit

---
+++ src/pages/posts/content.md
@@ -0,0 +1,21 @@
+---
+title: Titulo de la pagina
+---
+
+<h1 class="text-xs">Esto es un ejemplo de un articulo</h1>
+
+Eu laborum pariatur qui aliqua dolore. Mollit qui do dolor laboris sit aliqua id. Id sit consequat ex est.
+
+## Subtitulo
+
+Consectetur cupidatat culpa eu cillum incididunt cillum.
+
+## Subtitulo 2
+
+Reprehenderit laborum do sit sit.
+
+```js
+function volar() {
+  console.log("Estoy volando");
+}
+```
---
+++ src/services/spacex.ts
@@ -0,0 +1,78 @@
+import { type Doc, type APISpaceXResponse } from "../types/api-variables";
+
+interface Props {
+  id: string;
+}
+
+export const getLaunchById = async ({ id }) => {
+  const res = await fetch(`https://api.spacexdata.com/v5/launches/${id}`);
+  const launch = (await res.json()) as Doc;
+  return launch;
+}
+
+export const getLatestLaunches = async () => {
+  try {
+    const res = await fetch(`https://api.spacexdata.com/v5/launches/query`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: {},
+        options: {
+          sort: {
+            date_unix: "asc",
+          },
+          limit: 160,
+        },
+      }),
+    });
+
+    const jsonData = await res.json();
+    const { docs: launches } = jsonData as APISpaceXResponse;
+
+    // Ordena los 10 lanzamientos al límite
+    const latestLaunches = launches.slice(-10);
+
+    // Ordena los lanzamientos de forma descendente
+    latestLaunches.sort((a, b) => b.flight_number - a.flight_number);
+
+    return latestLaunches;
+  } catch (error) {
+    throw new Error("Error al obtener los lanzamientos");
+  }
+};
+
+export const getOlderLaunches = async () => {
+  try {
+    const res = await fetch(`https://api.spacexdata.com/v5/launches/query`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: {},
+        options: {
+          sort: {
+            date_unix: "asc",
+          },
+          limit: 160,
+        },
+      }),
+    });
+
+    const jsonData = await res.json();
+    const { docs: launches } = jsonData as APISpaceXResponse;
+
+    // Ordena los 10 lanzamientos al límite
+    const latestLaunches = launches.slice(-10);
+
+    // Ordena los lanzamientos de forma descendente
+    latestLaunches.sort((a, b) => b.flight_number - a.flight_number);
+
+    return latestLaunches;
+  } catch (error) {
+    throw new Error("Error al obtener los lanzamientos");
+  }
+}
+
