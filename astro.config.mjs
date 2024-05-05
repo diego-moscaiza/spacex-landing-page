de311eaa5b5c1c487b9433b510793eeec3816451
Diego Moscaiza <diego-moscaiza@outlook.com>
2024-05-05 14:12:03 -0500

    UI Improved and deleted comments

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,22 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  prefetch: {
+    defaultStrategy: "viewport",
+  },
   integrations: [tailwind()],
   image: {
-    remotePatterns: [{
-      protocol: "https"
-    }]
+    remotePatterns: [
+      {
+        protocol: "https",
+      },
+    ],
   },
   output: "hybrid",
   adapter: node({
-    mode: "standalone"
-  })
+    mode: "standalone",
+  }),
 });
